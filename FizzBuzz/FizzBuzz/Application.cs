7da5a52ac07b6addd4e3a110bde6b35728a6d25c
javflores <jflores@findmypast.com>
2016-05-01 11:23:19 +0100

    Adding application as a simple entry point for the console app

---
+++ FizzBuzz/FizzBuzz/Application.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            var fizzBuzz = new FizzBuzz();
+            Console.Write(fizzBuzz.Get(100));
+            Console.ReadKey();
+        }
+    }
+}
