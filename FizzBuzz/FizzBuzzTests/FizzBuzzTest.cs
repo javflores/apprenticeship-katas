25020fba2924be066cbbf1863316e1a72540fb08
javflores <jflores@findmypast.com>
2016-05-01 09:10:07 +0100

    including modified files, this is me learning git

--- FizzBuzz/FizzBuzzTests/FizzBuzzTest.cs
+++ FizzBuzz/FizzBuzzTests/FizzBuzzTest.cs
@@ -1,6 +1,27 @@
-﻿namespace FizzBuzzTests
+﻿using Xunit;
+
+namespace FizzBuzzTests
 {
     public class FizzBuzzTest
     {
+        [Fact]
+        public void FizzBuzz_for_one_element()
+        {
+            var fizzBuzz = new FizzBuzz.FizzBuzz();
+
+            string fizzBuzzSequence = fizzBuzz.Get(1);
+
+            Assert.Equal("1", fizzBuzzSequence);
+        }
+
+        [Fact]
+        public void FizzBuzz_for_two_elements()
+        {
+            var fizzBuzz = new FizzBuzz.FizzBuzz();
+
+            string fizzBuzzSequence = fizzBuzz.Get(2);
+
+            Assert.Equal("1 2", fizzBuzzSequence);
+        }
     }
 }
