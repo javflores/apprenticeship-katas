b0afb7db6b304f60e519e1bd42f536d3d9a82be8
javflores <jflores@findmypast.com>
2016-05-01 09:04:15 +0100

    Second test, fizzbuzz for two elements

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Get(int numberOfElements)
+        {
+            var integerSequence = Enumerable.Range(1, numberOfElements);
+
+            return string.Join(" ", integerSequence);
+        }
+    }
+}
