30eafc2d386803af4fd8558f9b08fda3a9a6ee75
javflores <jflores@findmypast.com>
2016-05-02 18:30:11 +0100

    Leap Year kata, using tests cases to simplify tests

--- LeapYear/LeapYear/LeapYearTests.cs
+++ LeapYear/LeapYear/LeapYearTests.cs
@@ -5,23 +5,13 @@ namespace LeapYear
     public class LeapYearTests
     {
         [Theory]
-        [InlineData(2001)]
-        [InlineData(1900)]
-        public void Common_year(int commonYear)
+        [InlineData(2001, false)]
+        [InlineData(1900, false)]
+        [InlineData(1996, true)]
+        [InlineData(2000, true)]
+        public void Common_year(int commonYear, bool isLeap)
         {
-            Assert.False(LeapYear.IsLeap(commonYear));
-        }
-
-        [Fact]
-        public void Typical_leap_year_is_divisible_by_4_and_not_divisible_by_100()
-        {
-            Assert.True(LeapYear.IsLeap(1996));
-        }
-
-        [Fact]
-        public void Atypical_Leap_year_is_divisible_by_400()
-        {
-            Assert.True(LeapYear.IsLeap(2000));
+            Assert.Equal(isLeap, LeapYear.IsLeap(commonYear));
         }
     }
 }
