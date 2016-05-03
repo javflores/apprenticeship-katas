17bf691408f61098210ed30bde708a06e97d3516
javflores <jflores@findmypast.com>
2016-05-03 06:02:13 +0100

    Bowling game kata, first test to get score of a game

---
+++ BowlingGame/BowlingGameTBowlingGame.cs
@@ -0,0 +1,10 @@
+namespace BowlingGame
+{
+    public class BowlingGame
+    {
+        public int Score(string game)
+        {
+            return 300;
+        }
+    }
+}
--- BowlingGame/BowlingGameTests/BowlingGameShould.cs
+++ BowlingGame/BowlingGameTests/BowlingGameShould.cs
@@ -1,7 +1,15 @@
-﻿namespace BowlingGameTests
+﻿using Xunit;
+
+namespace BowlingGameTests
 {
     public class BowlingGameShould
     {
-
+        [Theory]
+        [InlineData("X|X|X|X|X|X|X|X|X|X||XX", 300)]
+        public void score_the_game(string game, int expectedScore)
+        {
+            var score = new BowlingGame.BowlingGame().Score(game);
+            Assert.Equal(expectedScore, score);
+        }
     }
 }
