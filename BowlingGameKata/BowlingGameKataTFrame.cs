f0266b8e137150889cbbd2d69565c9f240d6dd86
javflores <jflores@findmypast.com>
2016-05-06 07:37:31 +0100

    Bowling game kata, creating Frame type and possibility to get Score of that frame. For now providing the individual throws of the next frame so that we can calculate the score of a frame

---
+++ BowlingGameKata/BowlingGameKataTFrame.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingGameKata
+{
+    public class Frame
+    {
+        private const char Strike = 'X';
+        private const char Spare = '/';
+        private readonly IList<char> _pinsKnocked;
+
+        public Frame(string frame)
+        {
+            _pinsKnocked = frame.ToCharArray();
+        }
+
+        public int Score(char[] pinsKnockedNextFrame)
+        {
+            if (_pinsKnocked.Contains(Spare))
+            {
+                return 10 + Score(pinsKnockedNextFrame[0]);
+            }
+
+            if (_pinsKnocked.Contains(Strike))
+            {
+                return 10 + NextFrameScore(pinsKnockedNextFrame);
+            }
+
+            return _pinsKnocked
+                .Select(ball => Score(ball))
+                .Aggregate((firstScore, secondScore) => firstScore + secondScore);
+        }
+
+        private int NextFrameScore(char[] pinsKnockedNextFrame)
+        {
+            if (pinsKnockedNextFrame.Contains(Spare) || pinsKnockedNextFrame.Contains(Strike))
+            {
+                return 10;
+            }
+
+            return Score(pinsKnockedNextFrame[0]) + Score(pinsKnockedNextFrame[1]);
+        }
+
+        private static int Score(char ball)
+        {
+            if (char.IsNumber(ball))
+            {
+                return (int) char.GetNumericValue(ball);
+            }
+
+            if (ball == Strike)
+            {
+                return 10;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ BowlingGameKata/BowlingGameKataTests/FrameShould.cs
@@ -0,0 +1,30 @@
+﻿using BowlingGameKata;
+using Xunit;
+
+namespace BowlingGameKataTests
+{
+    public class FrameShould
+    {
+        [Theory]
+        [InlineData("12", new[] { '1', '1' }, 3)]
+        [InlineData("1-", new[] { '1', '1' }, 1)]
+        [InlineData("2-", new[] { '1', '1' }, 2)]
+        [InlineData("-2", new[] { '1', '1' }, 2)]
+        [InlineData("-2", new[] { 'X' }, 2)]
+        [InlineData("1/", new [] { '1', '1' }, 11)]
+        [InlineData("1/", new[] { '1', '-' }, 11)]
+        [InlineData("1/", new[] { 'X' }, 20)]
+        [InlineData("1/", new[] { '-', '1' }, 10)]
+        [InlineData("1/", new[] { '-', '-' }, 10)]
+        [InlineData("X", new[] { '1', '1' }, 12)]
+        [InlineData("X", new[] { '-', '1' }, 11)]
+        [InlineData("X", new[] { '1', '/' }, 20)]
+        [InlineData("X", new[] { 'X' }, 20)]
+        public void have_a_score(string frame, char[] followingFrame, int expectedScore)
+        {
+            int frameScore = new Frame(frame).Score(followingFrame);
+
+            Assert.Equal(expectedScore, frameScore);
+        }
+    }
+}
