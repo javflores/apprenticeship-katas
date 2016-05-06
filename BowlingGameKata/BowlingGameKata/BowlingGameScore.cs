2047b57eb881ccc94e8477066a26fabd4f2f9727
javflores <jflores@findmypast.com>
2016-05-06 05:59:38 +0100

    Bowling game kata, when a frame is a spare, the score is 10 plus the score of the next ball

--- BowlingGameKata/BowlingGameKata/BowlingGameScore.cs
+++ BowlingGameKata/BowlingGameKata/BowlingGameScore.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace BowlingGameKata
@@ -9,9 +10,13 @@ namespace BowlingGameKata
         {
             var mainFrames = gameResult.Split(new []{"||"}, StringSplitOptions.RemoveEmptyEntries);
             var frames = mainFrames[0].Split('|');
-            if (frames.All(f => f.Contains("1")))
+            var ballsForFrames = frames.Select(frame => frame.ToCharArray());
+
+            if (frames.All(ContainsNumber))
             {
-                return 10;
+                return ballsForFrames
+                    .Select(f => ToFrameScore(f))
+                    .Aggregate((firstFrame, secondFrame) => firstFrame + secondFrame);
             }
 
             if (gameResult.Length < 23)
@@ -20,6 +25,38 @@ namespace BowlingGameKata
             }
 
             return 300;
+        }
+
+        private int ToFrameScore(char[] frame)
+        {
+            if (frame.Contains("/"))
+            {
+                return 10;
+            }
+
+            return frame
+                .Select(ToBallScore)
+                .Aggregate((firstScore, secondScore) => firstScore + secondScore);
+        }
+
+        private int ToBallScore(char ball)
+        {
+            if (IsNumber(ball))
+            {
+                return (int) char.GetNumericValue(ball);
+            }
+
+            return 0;
+        }
+
+        private bool ContainsNumber(string frame)
+        {
+            return frame.ToCharArray().Any(x => IsNumber(x));
+        }
+
+        private bool IsNumber(char x)
+        {
+            return char.IsNumber(x);
         }
     }
 }
--- BowlingGameKata/BowlingGameKataTests/BowlingGameScoreShould.cs
+++ BowlingGameKata/BowlingGameKataTests/BowlingGameScoreShould.cs
@@ -10,6 +10,9 @@ namespace BowlingGameKataTests
         [InlineData("X|X|X|X|X|X|X|X|X|1-||", 91)]
         [InlineData("1-|1-|1-|1-|1-|1-|1-|1-|1-|1-||", 10)]
         [InlineData("2-|2-|2-|2-|2-|2-|2-|2-|2-|2-||", 20)]
+        [InlineData("-2|-1|2-|2-|2-|2-|-1|2-|2-|2-||", 18)]
+        [InlineData("52|51|26|22|23|24|41|25|21|23||", 56)]
+        [InlineData("1/|11|11|11|11|11|11|11|11|11||", 29)]
         public void calculate_score(string gameResult, int expectedScore)
         {
             int score = new BowlingGameScore().Calculate(gameResult);
