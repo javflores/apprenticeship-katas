72e886cc5b4e37a0af6a2cf974ffcc9abb86d1b8
javflores <jflores@findmypast.com>
2016-05-10 08:21:23 +0100

    Bowling game kata, reworking kata to use number of frames as an invariant and looping through the balls WIP

--- BowlingGameKata/BowlingGameKata/BowlingGameScore.cs
+++ BowlingGameKata/BowlingGameKata/BowlingGameScore.cs
@@ -1,29 +1,82 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace BowlingGameKata
 {
     public class BowlingGameScore
     {
+        private string _bowlerThrows;
+
         public int Calculate(string bowlerThrows)
         {
-            var throws = bowlerThrows
-                .Split(new []{"||"}, StringSplitOptions.RemoveEmptyEntries);
-
-            var balls = bowlerThrows.ToCharArray().Where(x => x != '|');
-
-            var mandatoryThrows = throws[0].Split('|');
+            _bowlerThrows = bowlerThrows;
 
             int score = 0;
-            for (var frameIndex = 0; frameIndex < mandatoryThrows.Length - 1; frameIndex++)
+            int frameIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
             {
-                score += new Frame(mandatoryThrows[frameIndex])
-                    .Score(mandatoryThrows[frameIndex + 1]);
+                if (IsStrike(frameIndex))
+                {
+                    score += 10 + StrikeBonus(frameIndex);
+                    frameIndex += 2;
+                }
+
+                else if (IsSpare(frameIndex))
+                {
+                    score += 10 + SpareBonus(frameIndex);
+                    frameIndex += 3;
+                }
+
+                else
+                {
+                    score += KnockedPins(frameIndex) + KnockedPins(frameIndex + 1);
+                    frameIndex += 3;
+                }
             }
 
-            score += new Frame(mandatoryThrows[mandatoryThrows.Length - 1]).Score("");
-
             return score;
+        }
+
+        private bool IsStrike(int frameIndex)
+        {
+            return _bowlerThrows[frameIndex] == 'X';
+        }
+
+        private int StrikeBonus(int frameIndex)
+        {
+            var secondBonusBallIndex = frameIndex + 3;
+            if (IsStrike(frameIndex + 2))
+            {
+                secondBonusBallIndex = frameIndex + 4;
+            }
+
+            return KnockedPins(frameIndex + 2) + KnockedPins(secondBonusBallIndex);
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return _bowlerThrows[frameIndex + 1] == '/';
+        }
+
+        private int SpareBonus(int frameIndex)
+        {
+            return KnockedPins(frameIndex + 3);
+        }
+
+        private int KnockedPins(int frameIndex)
+        {
+            if (_bowlerThrows[frameIndex] == '-')
+            {
+                return 0;
+            }
+
+            if (IsStrike(frameIndex))
+            {
+                return 10;
+            }
+
+            return (int)char.GetNumericValue(_bowlerThrows[frameIndex]);
         }
     }
 }
--- BowlingGameKata/BowlingGameKataTests/BowlingGameScoreShould.cs
+++ BowlingGameKata/BowlingGameKataTests/BowlingGameScoreShould.cs
@@ -6,12 +6,16 @@ namespace BowlingGameKataTests
     public class BowlingGameScoreShould
     {
         [Theory]
-        [InlineData("X|X|X|X|X|X|X|X|X|1-||", 252)]
-        [InlineData("1-|1-|1-|1-|1-|1-|1-|1-|1-|1-||", 10)]
-        [InlineData("2-|2-|2-|2-|2-|2-|2-|2-|2-|2-||", 20)]
-        [InlineData("-2|-1|2-|2-|2-|2-|-1|2-|2-|2-||", 18)]
-        [InlineData("52|51|26|22|23|24|41|25|21|23||", 56)]
+        [InlineData("--|--|--|--|--|--|--|--|--|--||", 0)]
+        [InlineData("11|11|11|11|11|11|11|11|11|11||", 20)]
         [InlineData("1/|11|11|11|11|11|11|11|11|11||", 29)]
+        [InlineData("1/|21|11|11|11|11|11|11|11|11||", 31)]
+        [InlineData("1/|21|11|11|11|11|11|--|11|11||", 29)]
+        [InlineData("1/|X|11|11|11|11|11|--|11|11||", 46)]
+        [InlineData("X|21|11|11|11|11|11|--|11|11||", 30)]
+        [InlineData("X|X|11|11|11|11|11|--|11|11||", 47)]
+        [InlineData("X|X|X|11|11|11|11|--|11|11||", 75)]
+        [InlineData("X|X|X|X|X|X|X|X|X|11||", 245)]
         public void calculate_score(string bowlerThrows, int expectedScore)
         {
             int score = new BowlingGameScore().Calculate(bowlerThrows);
